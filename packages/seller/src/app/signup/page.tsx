1ff73c395b052930413701d9be4bf60bfed2f09d
rakicodes <goraquel22@gmail.com>
2024-06-12 23:43:57 -0600

    clean

--- packages/seller/src/app/signup/page.tsx
+++ packages/seller/src/app/signup/page.tsx
@@ -1,42 +1,49 @@
-"use client"
-import React, { useState } from 'react'
-import SignupTemplate from '@ui/templates/SignupTemplate'
+"use client";
+import React, { useState } from "react";
+import SignupTemplate from "@ui/templates/SignupTemplate";
 
 const Page = () => {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleSubmit = async () => {
-    console.log("submitting...")
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/signup`, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password
-      })
-    })
-    const data = await res.json()
-    console.log(data)
-  }
+	const handleSubmit = async () => {
+		const res = await fetch(
+			`${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/signup`,
+			{
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({
+					name,
+					email,
+					password,
+				}),
+			}
+		);
+		const data = await res.json();
+	};
 
-  return (
-    <>
-      <SignupTemplate 
-        name={name}
-        email={email}
-        password={password}
-        onChangeName={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-        onChangeEmail={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-        onChangePassword={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-        onSubmit={handleSubmit}
-      />
-    </>
-  )
-}
+	return (
+		<>
+			<SignupTemplate
+				name={name}
+				email={email}
+				password={password}
+				onChangeName={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setName(e.target.value)
+				}
+				onChangeEmail={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setEmail(e.target.value)
+				}
+				onChangePassword={(e: React.ChangeEvent<HTMLInputElement>) =>
+					setPassword(e.target.value)
+				}
+				onSubmit={handleSubmit}
+			/>
+		</>
+	);
+};
 
-export default Page
+export default Page;

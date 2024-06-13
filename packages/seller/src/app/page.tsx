e48b5e6bd837103cf995eca7af8fc5191b5c2a96
rakicodes <goraquel22@gmail.com>
2024-06-12 21:15:00 -0600

    remove signup from home

--- packages/seller/src/app/page.tsx
+++ packages/seller/src/app/page.tsx
@@ -1,9 +1,6 @@
-import SignupInputs from "@ui/organisms/SignupInputs"
-
 export default function Home() {
   return (
     <>
-      <SignupInputs />
     </>
   );
 }
---
+++ packages/ui/templates/SignupTemplate.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignupTemplate = () => {
+  return (
+    <div>SignupTemplate</div>
+  )
+}
+
+export default SignupTemplate

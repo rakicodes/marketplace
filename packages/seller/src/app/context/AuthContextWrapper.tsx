e69d0c6241f8b837032e10cc964e4414947ffd78
rakicodes <goraquel22@gmail.com>
2024-06-14 22:15:06 -0600

    fix auth

--- packages/seller/src/app/context/AuthContextWrapper.tsx
+++ packages/seller/src/app/context/AuthContextWrapper.tsx
@@ -1,13 +1,17 @@
 "use client"
 import React from "react";
 import { IAuth } from "@ui/types/data";
-import { useState } from "react";
-import { setCookie, deleteCookie } from "cookies-next";
+import { useState, useEffect } from "react";
+import { setCookie, deleteCookie, getCookie } from "cookies-next";
 import { AuthContext } from "@/app/context/auth";
 import { IPropsWithOnlyChildren } from "@ui/types/props";
 
 const AuthContextWrapper = ({ children }: IPropsWithOnlyChildren) => {
 	const [currentUser, setCurrentUser] = useState<IAuth | null>(null);
+
+	useEffect(() => {
+		setCurrentUser(getCookie("auth") ? JSON.parse(getCookie("auth") || "") as IAuth : null)
+	}, [])
 
 	const handleAuthCookie = (user: IAuth | null) => {
 		setCurrentUser(user);

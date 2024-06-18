df1789949f4e396bbda2721115749cbea589503f
rakicodes <goraquel22@gmail.com>
2024-06-18 11:22:25 -0600

    change mobile search section size

--- packages/ui/templates/ListingsTemplate.tsx
+++ packages/ui/templates/ListingsTemplate.tsx
@@ -11,7 +11,7 @@ const ListingsTemplate = ({
 }: IListingsTemplate) => {
 	return (
 		<div className="flex flex-col min-h-screen pt-16 md:pt-6 pb-6 gap-y-2">
-			<div className="flex flex-col px-4 md:px-8 w-full bg-slate-900 h-72 justify-center gap-y-6">
+			<div className="flex flex-col px-4 md:px-8 w-full bg-slate-900 h-48 md:h-72 justify-center gap-y-6">
 				<SearchSection
 					value={searchValue}
 					onChange={onChange}

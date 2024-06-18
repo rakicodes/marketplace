f6093a9d36a687c96df8e87ad244a698922b76fd
rakicodes <goraquel22@gmail.com>
2024-06-18 10:01:30 -0600

    error and loading pages

--- packages/buyer/src/app/not-found.tsx
+++ packages/buyer/src/app/not-found.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import ErrorPageTemplate from '@ui/templates/ErrorPageTemplate'
 import { useRouter } from "next/navigation"
 
-const Error = () => {
+const NotFound = () => {
   const router = useRouter()
   return (
     <>
-      <ErrorPageTemplate link={() => router.push("/")} />
+      <ErrorPageTemplate link={() => router.push("/")} message="Sorry we couldn't find the page you're looking for" />
     </>
   )
 }
 
-export default Error
+export default NotFound

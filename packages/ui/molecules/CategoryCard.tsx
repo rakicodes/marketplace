27a2ea449ac20509155b0791de095e76ac81c4ef
rakicodes <goraquel22@gmail.com>
2024-06-18 11:42:55 -0600

    remove console log

--- packages/ui/molecules/CategoryCard.tsx
+++ packages/ui/molecules/CategoryCard.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ICategoryCard } from "../types/props";
 
 const CategoryCard = ({ category, onClick }: ICategoryCard) => {
-	console.log(category)
 	return (
 		<div className="cursor-pointer rounded-lg p-4 bg-slate-500 flex items-center justify-center min-w-32" onClick={onClick}>
 			<span className="text-white">{category}</span>
